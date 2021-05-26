a4c50493fc961d1671b889c7492d768913922f52
Rabi Shrestha (Smith) <81063938+srabi21@users.noreply.github.com>
2021-05-26 22:27:51 +0545

    Update products.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 
-
-<div style="padding:30px; background-color:#9deb0e;">
+<div style="padding:80px; background-color:#fcf8e0; background: -webkit-linear-gradient(to right, #CFDEF3, #E0EAFC);  
+background: linear-gradient(to right, #CFDEF3, #E0EAFC); ">
 
        
 <h2>CDs:</h2>
@@ -17,19 +17,20 @@ First Name: {{$product->getFirstName()}} {{$product->getMainName()}}
 
 PlayLength:{{$product->getPlayLength()}}
 <br>
-<a href="{{url('products/'. $product->getId())}}">Select</a>
-
+<button> <a href="{{url('products/'. $product->getId())}}">Select</a> </button>
+<br>
+<br>
 
     
-    <form method="post" action="{{route('product.delete',$product->getId())}}">
-                        @csrf
-                        @method('delete')
-                            <button type="submit"> Delete </button>
-                    </form>
+<form method="post" action="{{route('product.delete',$product->getId())}}">
+@csrf
+@method('delete')
+<button type="submit"> Delete </button>
+</form>
 <br>
 @endforeach
            
-
+<hr>
 
            
 <h2>Books:</h2>
@@ -44,36 +45,45 @@ First Name: {{$product->getFirstName()}} {{$product->getMainName()}}
 No. of pages:{{$product->getNumberOfPages()}}
 
 <br>
-<a href="{{url('products/'. $product->getId())}}"><span class="btn-btn-primary">select</span></a>
+<button> 
+    <a href="{{url('products/'. $product->getId())}}"><span class="btn-btn-primary">Select</span></a> </button>
+<br>
+<br>
 
    
     <form method="post" action="{{route('product.delete',$product->getId())}}">
-                        @csrf
-                        @method('delete')
-                            <button type="submit"> Delete </button>
+                    @csrf
+                    @method('delete')
+                         <button type="submit"> Delete </button>
                     </form>
     
  
+ <hr>
 
 
 @endforeach
             
- <h2>Add Product</h2> 
+<h2>Add Product</h2> 
 <form method="POST" action="/products/store" >
 @csrf
 
 <span ><strong>Product Type:</strong></span>
 <select name="type" >
     <option value="cd">CD</option>
-    <option value="book">BOOk</option>
+    <option value="book">BOOK</option>
     
 </select><br>
 <br>
 <input type="text" name="title" placeholder="Title" required><br>
+<br>
 <input type="text" name="firstname" placeholder="First Name"><br>
+<br>
 <input type="text" name="surname" placeholder="Surname"><br>
+<br>
 <input type="text" name="price" placeholder="Price"><br>
+<br>
 <input type="text" name="papl" placeholder="Pages/Play Length/PaPl"><br>
+<br>
 <button type="submit" name="" class="btn btn-success">SAVE</button>
 
 </form>
