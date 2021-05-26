6a726684b204e675bfcf89fb3e292b74c6452ecb
Rabi Shrestha (Smith) <81063938+srabi21@users.noreply.github.com>
2021-05-26 22:21:38 +0545

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Product;
+use App\Http\Controllers\BookProduct;
+use App\Http\Controllers\CdProduct;
+use App\Http\Controllers\ShopProduct;
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,5 +24,10 @@ Route::get('/products/{id}', [Product::class,'getProductWithId']);
 Route::post('/products/store', [Product::class,'addNewProduct']);
 Route::delete('delete-product/{id}',[Product::class,'deleteProductWithId'])->name('product.delete');
 Route::put('update-product/{id}',[Product::class,'updateProductWithId'])->name('product.update');
+Route::get('/', [Product::class,'makeProductArray']);
+ // Route::view('/', 'welcome');
 
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
