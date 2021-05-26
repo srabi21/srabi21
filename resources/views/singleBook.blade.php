bcda41f850209f314a83bd5345a7c0bca8108f7f
Rabi Shrestha (Smith) <81063938+srabi21@users.noreply.github.com>
2021-05-26 22:28:46 +0545

    Update singleBook.blade.php

--- resources/views/singleBook.blade.php
+++ resources/views/singleBook.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
-
+<div style="padding:80px; background-color:#fcf8e0; background: -webkit-linear-gradient(to right, #CFDEF3, #E0EAFC);  
+background: linear-gradient(to right, #CFDEF3, #E0EAFC); ">
 
 <div style="padding:30px;">
 <h2>Books:</h2>
@@ -22,21 +23,27 @@ Price:{{ $product->getPrice()}}
 <br>
 
 <!-- update form -->
+
 <form method="POST" action="{{route('product.update',$product->getId())}}" >
 @csrf
 @method('PUT')
 
 <span ><strong>Product Type:</strong></span>
 <select name="type">
-<option value="book">BOOk</option>
+<option value="book">BOOK</option>
 
 <br>    
 </select><br>
 <input  type="text" name="title"  value="{{$product->getTitle()}}" required><br>
+<br>
 <input  type="text" name="fname"  value="{{$product->getFirstName()}}"><br>
+<br>
 <input type="text" name="sname"  value="{{$product->getMainName()}}"><br>
+<br>
 <input type="text" name="price"  value="{{$product->getPrice()}}"><br>
+<br>
 <input  type="text" name="papl" value="{{$product->getNumberOfPages()}}"><br>
+<br>
 <button  type="submit" name="" class="btn btn-success">SAVE</button>
 
 </form>
