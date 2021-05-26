e2bbc0a4a878f1e62d137b95f2309367b02280f0
Rabi Shrestha (Smith) <81063938+srabi21@users.noreply.github.com>
2021-05-26 22:29:08 +0545

    Update singleCd.blade.php

--- resources/views/singleCd.blade.php
+++ resources/views/singleCd.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
-
+<div style="padding:80px; background-color:#fcf8e0; background: -webkit-linear-gradient(to right, #CFDEF3, #E0EAFC);  
+background: linear-gradient(to right, #CFDEF3, #E0EAFC); ">
 
 <div style="padding:30px;">
 <h2>CDs:</h2>
@@ -35,10 +36,15 @@ Price:{{ $product->getPrice()}}
 <br>    
 </select><br>
 <input  type="text" name="title" placeholder="Title" value="{{$product->getTitle()}}" required><br>
+<br>
 <input  type="text" name="fname" placeholder="First Name" value="{{$product->getFirstName()}}"><br>
+<br>
 <input  type="text" name="sname" placeholder="Surname" value="{{$product->getMainName()}}"><br>
+<br>
 <input  type="text" name="price" placeholder="Price" value="{{$product->getPrice()}}"><br>
+<br>
 <input  type="text" name="papl" placeholder="Pages/Play Length/PaPl" value="{{$product->getPlayLength()}}"><br>
+<br>
 <button  type="submit" name="" class="btn btn-success">UPDATE</button>
 
 </form>
